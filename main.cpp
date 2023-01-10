597a6022a0f753a21a91875a8076586818ba25d2
Mike Croteau <croteau.mike@gmail.com>
2023-01-10 03:15:51 -0900

    [init] initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(){
+
+    /*
+        read file
+        if annotation
+            create annotation object
+        if method
+            create method object
+     */
+
+    cout << "hi";
+
+    ifstream sources("sandbox");
+
+
+    const std::filesystem::path sandbox{"output"};
+    std::filesystem::create_directories(sandbox);
+
+    std::cout << "directory_iterator:\n";
+    // directory_iterator can be iterated using a range-for loop
+    for (auto dir_entry : std::filesystem::directory_iterator{sandbox})
+    {
+        std::cout << dir_entry.path() << '\n';
+    }
+
+
+    // create files of different kinds
+    fs::create_directory("output");
+
+
+    // cleanup
+    fs::remove_all("sandbox");
+}
---
+++ meta_object.cpp
@@ -0,0 +1,11 @@
+//
+// Created by mcroteau on 1/10/23.
+//
+#include <string>
+#include <map>
+
+class meta_object {
+    std::string type;
+    std::string element;
+    std::map<std::string, std::string> definitions;
+};
